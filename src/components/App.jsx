b9f3e810ec5032bd73a93956b7dc0199d8a11674
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-14 03:37:59 +0300

    Place navbar and footer at top,bot

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
+import styles from "./App.module.css";
 
 function App() {
   // I will put footer, topnavbar here
   return (
-    <>
-      <nav>Nav here</nav>
-      <Outlet />
-      <footer>Footer here</footer>
-    </>
+    <div className={styles.app}>
+      <nav className={styles.nav}>Nav here</nav>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+      <footer className={styles.footer}>Footer here</footer>
+    </div>
   );
 }
 
