17dc6528c766989a9b289bf03f3b049677db9f53
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-14 04:38:19 +0300

    Download images for home, style Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.imgBg}>
-      <h1>Home goes here</h1>
+    <div className={styles.home}>
+      <div className={styles.imgBg}>
+        <div className={styles.motivator}>
+          <h1>Some random text</h1>
+          <Link to="/shop">Shop now</Link>
+        </div>
+
+        <div className="points"></div>
+        <div className="reviews"></div>
+        <div className="leave-review"></div>
+      </div>
     </div>
   );
 }
