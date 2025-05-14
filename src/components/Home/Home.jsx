332125570c9e704318cc90ad39c57e9b81560d89
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-14 06:35:08 +0300

    Render reviews in /home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,36 +1,8 @@
 import { Link } from "react-router-dom";
+import Reviews from "./components/Reviews";
 import styles from "./Home.module.css";
-import { useState, useEffect } from "react";
 
 export default function Home() {
-  const FetchLocalReviews = () => {
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-      fetch("../../../data/Home/reviews.json")
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Failed to fetch reviews");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setData(data);
-          setLoading(false);
-        })
-        .catch((err) => {
-          setError(err.message);
-          setLoading(false);
-        });
-    }, []);
-
-    return { error, loading, data };
-  };
-
-  const { error, loading, data } = FetchLocalReviews();
-
   return (
     <div className={styles.home}>
       <div className={styles.imgBg}>
@@ -38,10 +10,10 @@ export default function Home() {
           <h1>Some random text</h1>
           <Link to="/shop">Shop now</Link>
         </div>
-        <div className="points"></div>
-        <div className="reviews"></div>
-        <div className="leave-review"></div>
       </div>
+      <Reviews />
+      <div className="points"></div>
+      <div className="leave-review"></div>
     </div>
   );
 }
---
+++ src/components/Home/components/Reviews.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import styles from "./Reviews.module.css";
+
+export default function Reviews() {
+  const FetchLocalReviews = () => {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      fetch("../../../data/Home/reviews.json")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to fetch reviews");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setData(data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false);
+        });
+    }, []);
+
+    return { error, loading, data };
+  };
+
+  const { error, loading, data } = FetchLocalReviews();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    data && (
+      <div className={styles.reviews}>
+        {data.map((person) => (
+          <div className={styles.reviewsEntry} key={person.id}>
+            <img src={person.photo} alt={`${person.name} photo`} />
+            <p>{person.review}</p>
+            <p>{person.score}</p>
+            <p>{person.name}</p>
+          </div>
+        ))}
+      </div>
+    )
+  );
+}
