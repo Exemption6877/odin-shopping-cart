f1520d75d831b07423f1303223ea90bb5c46c930
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-14 05:12:11 +0300

    Create custom hook for local reviews fetch

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,37 @@
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const FetchLocalReviews = () => {
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      fetch("../../../data/Home/reviews.json")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to fetch reviews");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setData(data);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setLoading(false);
+        });
+    }, []);
+
+    return { error, loading, data };
+  };
+
+  const { error, loading, data } = FetchLocalReviews();
+  console.log(data);
+
   return (
     <div className={styles.home}>
       <div className={styles.imgBg}>
@@ -9,7 +39,6 @@ export default function Home() {
           <h1>Some random text</h1>
           <Link to="/shop">Shop now</Link>
         </div>
-
         <div className="points"></div>
         <div className="reviews"></div>
         <div className="leave-review"></div>
