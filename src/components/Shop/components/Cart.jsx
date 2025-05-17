032ba040c5bffc0299c835a93a4495f74dbb1017
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-17 06:01:10 +0300

    Style cart, count total sum

--- src/components/Shop/components/Cart.jsx
+++ src/components/Shop/components/Cart.jsx
@@ -1,28 +1,44 @@
 import styles from "./Cart.module.css";
 
 export default function Cart({ selected, setShowCart }) {
+  const back_svg = "./icons/shop/back.svg";
+
+  const totalValue = selected.reduce(
+    (acc, item) => acc + item.amount * item.price,
+    0
+  );
+
   return (
     <>
       <div
         className={styles.background}
         onClick={() => setShowCart(false)}
       ></div>
+
       <div className={styles.cart}>
+        <button className={styles.back} onClick={() => setShowCart(false)}>
+          <img src={back_svg} alt="Go back" />
+        </button>
         <div className={styles.grid}>
+          <h2>Items</h2>
           {selected.length > 0 ? (
             selected.map((entry) =>
               entry.amount > 0 ? (
                 <div key={entry.id} className={styles.item}>
                   <img src={entry.images} alt={entry.title} />
                   <p>{entry.title}</p>
-                  <h4>${entry.price}</h4>
-                  <h4>Amount: {entry.amount}</h4>
+                  <div className={styles.purchaseDetails}>
+                    <h4>{entry.amount}x</h4>
+                    <h4>${entry.price}</h4>
+                  </div>
                 </div>
               ) : null
             )
           ) : (
             <p>No entries yet!</p>
           )}
+          <h3 className={styles.total}>Total: ${totalValue}</h3>
+          <button className={styles.submit}>Submit!</button>
         </div>
       </div>
     </>
